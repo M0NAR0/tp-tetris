1b31b45b36489892424bd175c34c1523b8d21bae
Morgan <morganlebeau53@gmail.com>
2020-09-24 12:16:48 +0200

    Removed useless DAO files

--- src/main/java/fr/formation/dao/IBlockDao.java
+++
@@ -1,7 +0,0 @@
-package fr.formation.dao;
-
-import fr.formation.model.Block;
-
-public interface IBlockDao extends IDAO<Block> {
-
-}
--- src/main/java/fr/formation/dao/IPieceDao.java
+++
@@ -1,7 +0,0 @@
-package fr.formation.dao;
-
-import fr.formation.model.Piece;
-
-public interface IPieceDao extends IDAO<Piece> {
-
-}
